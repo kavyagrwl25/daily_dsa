7ce092b1b1d25e957506cceef1bd79bb5f1e6bb4
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-07-11 08:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (34.97%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+//kavy
+    vector<int> plusOne(vector<int>& digits) {
+    int n = digits.size();
+    
+    for (int i = n - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0; // carry over
+    }
+    
+    // If all digits were 9, e.g., 999 + 1 = 1000
+    digits.insert(digits.begin(), 1);
+    return digits;
+}
+};
