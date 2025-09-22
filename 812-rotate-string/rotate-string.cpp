6c29c42213e95c95032a50d474521537eeaa1504
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-09-22 21:17:53 +0530

    Time: 4 ms (1.45%) | Memory: 9.7 MB (6.26%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -8,12 +8,12 @@ public:
 
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
-        string newS = s;
+        /* string newS = s; */
         for (int i = 0; i<s.size(); i++) {
-            if (newS == goal) {
+            if (s == goal) {
                 return true;
             }
-            newS = rightRotateByOne(newS);
+            s = rightRotateByOne(s);
         }
         return false;
     }
