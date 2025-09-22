ef3857f0fac0b609d41623dbbba790e9fc78187c
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-09-22 21:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (36.82%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
-    string rightRotateByOne(string s) {
-        if (s.empty())
-            return s;
-        return s.back() + s.substr(0, s.size() - 1);
-    }
-
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return false;
-        string newS = s;
-        for (int i = 0; i<s.size(); i++) {
-            if (newS == goal) {
-                return true;
-            }
-            newS = rightRotateByOne(newS);
+        if (s.length() != goal.length()) {
+            return false;
         }
-        return false;
+        return (s + s).find(goal) != string::npos;
     }
 };
