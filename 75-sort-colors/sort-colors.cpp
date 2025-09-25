18d69ec8a1d1b345b21a2060023e0723f98b65f8
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-09-25 23:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.15%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -10,7 +10,7 @@ public:
                 mid++;
             } else if (nums[mid] == 1) {
                 mid++;
-            } else { // nums[mid] == 2
+            } else { 
                 swap(nums[mid], nums[high]);
                 high--;
             }
