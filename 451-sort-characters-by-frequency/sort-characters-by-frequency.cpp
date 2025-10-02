f01584c93ae4d88a5d1117f11c199599fdd90eda
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-02 16:58:12 +0530

    Time: 8 ms (13.12%) | Memory: 11 MB (63.89%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> freq;
+        vector<pair<int, char>> freq;
 
-        for (char c : s) {
-            freq[c]++;
+        
+        for (int i = 0; i < 128; i++) {
+            freq.push_back({0, char(i)});
         }
 
-        sort(s.begin(), s.end(), [&](char a, char b) {
-            if (freq[a] == freq[b])
-                return a < b;
-            return freq[a] > freq[b];
+       
+        for (char c : s) {
+            freq[c].first++;
+        }
+
+        
+        sort(freq.begin(), freq.end(), [](auto& a, auto& b) {
+            return a.first > b.first; 
         });
 
-        return s;
+        string ans = "";
+        for (int i = 0; i < 128; i++) {
+            while (freq[i].first > 0) {
+                ans += freq[i].second;
+                freq[i].first--;
+            }
+        }
+
+        return ans;
     }
 };
+
+/* unordered_map<char, int> freq;
+
+for (char c : s) {
+    freq[c]++;
+}
+
+sort(s.begin(), s.end(), [&](char a, char b) {
+    if (freq[a] == freq[b])
+        return a < b;
+    return freq[a] > freq[b];
+});
+
+return s; */
