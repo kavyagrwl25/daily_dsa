7d9277f033b69e5ec25929428268f6baa6c5e8a1
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-02 17:03:47 +0530

    Time: 3 ms (54.32%) | Memory: 11.4 MB (40.34%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,27 +3,22 @@ public:
     string frequencySort(string s) {
         vector<pair<int, char>> freq;
 
-        
         for (int i = 0; i < 128; i++) {
             freq.push_back({0, char(i)});
         }
 
-       
         for (char c : s) {
             freq[c].first++;
         }
 
-        
-        sort(freq.begin(), freq.end(), [](auto& a, auto& b) {
-            return a.first > b.first; 
-        });
+        sort(freq.begin(), freq.end(),
+             [](auto& a, auto& b) { return a.first > b.first; });
 
+        
         string ans = "";
         for (int i = 0; i < 128; i++) {
-            while (freq[i].first > 0) {
-                ans += freq[i].second;
-                freq[i].first--;
-            }
+            if (freq[i].first > 0)
+                ans.append(freq[i].first, freq[i].second);
         }
 
         return ans;
