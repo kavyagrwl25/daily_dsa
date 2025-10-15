54cbde54080aecc3a6b9173106f7986618000d60
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-15 14:48:49 +0530

    Time: 7 ms (97.79%) | Memory: 60.1 MB (86.07%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0) return 0;
-        int maxCount = 1;
+        if(nums.size() == 0) return 0;
         sort(nums.begin(), nums.end());
-        int count=1; 
-        for(int i=1; i<n; i++){
-            if(nums[i-1] == nums[i]){
+        int count = 1;
+        int result = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] == nums[i])
                 continue;
-            }
-            else if(nums[i-1] == nums[i]-1){
+            if (nums[i - 1] + 1 == nums[i]) {
+
                 count++;
-                maxCount = max(maxCount, count);
-            }
-            else{
+                result = max(result, count);
+            } else {
                 count = 1;
             }
         }
-        return maxCount;
+        return result;
     }
 };
+
+/*
+
+
+
+int n = nums.size();
+int count = 1;
+int maxCount = 1;
+if (n == 0)
+    return 0;
+sort(nums.begin(), nums.end());
+for (int i = 1; i < n; i++) {
+    if (nums[i - 1] == nums[i])
+        continue;
+    if (nums[i - 1] == nums[i] - 1) {
+        count++;
+        maxCount = max(maxCount, count);
+    } else if (nums[i - 1] != nums[i]) {
+        count = 1;
+    }
+}
+return maxCount; */
