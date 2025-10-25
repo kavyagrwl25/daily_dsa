6a3982300de9728f041c919288df20adc1e718b5
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-25 17:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+    /* int totalMoney(int n) { */
+    /* int count = 0;
+    int ans = 0;
+    int j=1;
+    int k =2;
+    for(int i=0; i<n; i++){
+        ans += j;
+        count++;
+        if(count == 7) {
+            j=k;
+            k++;
+            count = 0;
+        }else {
+        j++;
+    }
+    }
+    return ans; */
+
     int totalMoney(int n) {
-        int count = 0;
         int ans = 0;
-        int j=1;
-        int k =2;
-        for(int i=0; i<n; i++){
-            ans += j;
-            count++;
-            if(count == 7) {
-                j=k;
-                k++;
-                count = 0;
-            }else {
-            j++;
-        }
+        int weekStart = 1;
+        for (int i = 0; i < n; i++) {
+            ans += weekStart + (i % 7);
+            if (i % 7 == 6)
+                weekStart++;
         }
         return ans;
     }
