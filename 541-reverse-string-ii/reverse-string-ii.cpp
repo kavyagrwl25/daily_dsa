9e2a134a6f77c36436c64c14e902757df1b0726e
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-25 18:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (76.71%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    void rev(string& x, int i, int j) {
+    /* void rev(string& x, int i, int j) {             //using recursion but taken O(k) space
         if (i > j)
             return;
         swap(x[i], x[j]);
 
         rev(x, i + 1, j - 1);
+    } */
+
+    void revIter(string& s, int i, int j) {            //using while loop taking O(1) space
+        while (i < j) {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
     }
 
     string reverseStr(string s, int k) {
         int n = s.size();
         for (int start = 0; start < n; start += 2 * k) {
             int end = min(start + k - 1, n - 1);
-            rev(s, start, end);
+            revIter(s, start, end);
         }
         return s;
     }
