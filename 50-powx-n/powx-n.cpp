e6f67561a4197d37adfe4526a1f58c81adf42a1c
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2025-10-30 22:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.65%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    /* double myPow(double x, int n) {
         long pow = n;
         if(pow < 0) {
             x= 1/x;
@@ -15,5 +15,24 @@ public:
             pow /= 2;
         }
         return ans;
+    } */
+
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = myPow(x, N / 2);
+        if (N % 2 == 0) {
+            return ans * ans;
+        } else {
+            return x * ans * ans;
+        }
     }
 };
