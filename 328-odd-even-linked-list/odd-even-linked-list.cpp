25245b1b81f6de9d83fb06013b1ddb26a4d98887
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-02-17 23:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (26.50%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,7 +11,25 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (!head) return head;
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* tempO = head;
+        ListNode* tempE = head->next;
+        ListNode* linkage = head->next;
+        
+        while(tempE != NULL && tempE->next != NULL){
+            tempO->next = tempO->next->next;
+            tempE->next = tempE->next->next;
+            tempO = tempO->next;
+            tempE = tempE->next;
+        }
+        tempO->next = linkage;
+        return head;
+    }
+};
+
+/* 
+
+if (!head) return head;
 
         vector<int> arr;
         ListNode* temp = head;
@@ -36,6 +54,4 @@ public:
             temp->val = arr[i];
             temp = temp->next;
         }
-        return head;
-    }
-};
+        return head; */
