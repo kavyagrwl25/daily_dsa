88b5e35b558b395756eaa14d3b47a1e81b40472c
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-02-19 11:26:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (58.80%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int cnt = 0;
+        ListNode* temp = head;
+        while (temp) {
+            cnt++;
+            temp = temp->next;
+        }
+        if (n == cnt) {
+            return head->next;
+        }
+        int ans = (cnt - n) - 1;
+        temp = head;
+        while (ans) {
+            temp = temp->next;
+            ans--;
+        }
+        temp->next = temp->next->next;
+        return head;
+    }
+};
