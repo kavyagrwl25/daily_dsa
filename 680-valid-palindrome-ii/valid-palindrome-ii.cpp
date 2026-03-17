380b36b55d3c07e6900503e6d2bf6cdada3d2298
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-03-17 22:38:50 +0530

    Time: 2 ms (75.47%) | Memory: 22 MB (80.15%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-
-    bool isPal(string &s, int left, int right){
-        while(left < right){
-            if(s[left] != s[right]) return false;
-            left++;
-            right--;
+    bool checkPalindrome(string& s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
         }
         return true;
     }
@@ -15,10 +15,11 @@ public:
         int i = 0;
         int j = s.size() - 1;
 
-        while(i < j){
+        while (i < j) {
 
-            if(s[i] != s[j]){
-                return isPal(s, i + 1, j) || isPal(s, i, j - 1);
+            if (s[i] != s[j]) {
+                return checkPalindrome(s, i + 1, j) ||
+                       checkPalindrome(s, i, j - 1);
             }
 
             i++;
