36b102cb6371fcbb7b0e37476e8fd6b3ea86afb1
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-04-04 21:22:45 +0530

    Time: 7 ms (20.02%) | Memory: 15.3 MB (15.53%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> stack;
-        string cur;
-
-        for (char c : path + "/") {
-            if (c == '/') {
-                if (cur == "..") {
-                    if (!stack.empty()) stack.pop_back();
-                } else if (!cur.empty() && cur != ".") {
-                    stack.push_back(cur);
-                }
-                cur.clear();
-            } else {
-                cur += c;
+        string token = "";
+        stack<string> st;
+        stringstream ss(path);
+        while(getline(ss, token, '/')){
+            if(token == ".."){
+                if(!st.empty())
+                    st.pop();
+            }
+            else if(token == "." || token == ""){
+                continue;
+            }else{
+                st.push(token);
             }
         }
-
-        string result = "/";
-        for (int i = 0; i < stack.size(); ++i) {
-            if (i > 0) result += "/";
-            result += stack[i];
+        string result = "";
+        while(!st.empty()){
+            result = "/" + st.top()+ result;
+            st.pop();
         }
-
-        return result;
+        return result == "" ? "/" : result;
     }
 };
