7332327b8db24f3314e17f15b76c079601795682
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-04-05 22:01:49 +0530

    Time: 2 ms (23.32%) | Memory: 9.6 MB (35.20%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        string ans = "";
+        vector<string> st;
+        for (char& c : s) {
+            if (c != ']') {
+                st.push_back(string(1, c));
+            } else {
+                string substr = "";
+                while (st.back() != "[") {
+                    substr = st.back() + substr;
+                    st.pop_back();
+                }
+                st.pop_back();
+                string a = "";
+                while (!st.empty() && isdigit(st.back()[0])) {
+                    a = st.back() + a;
+                    st.pop_back();
+                }
+                int k = stoi(a);
+                string repeat = "";
+                while (k--)
+                    repeat += substr;
+                st.push_back(repeat);
+            }
+        }
+        for (string &part : st) {
+            ans += part;
+        }
+        return ans;
+    }
+};
