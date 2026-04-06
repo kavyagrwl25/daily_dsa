32a2ccfc88a7e47e1498627ff01d09361fa0f5ea
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-04-06 16:36:31 +0530

    Time: 1 ms (18.79%) | Memory: 8.8 MB (66.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 1) return false;
         stack<char> st;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
@@ -10,10 +9,11 @@ public:
                 if (st.empty())
                     return false;
                 char ch = st.top();
-                st.pop();
+                
 
                 if ((ch == '(' && s[i] == ')') || (ch == '[' && s[i] == ']') ||
                     (ch == '{' && s[i] == '}')) {
+                        st.pop();
                     continue;
                 } else {
                     return false;
