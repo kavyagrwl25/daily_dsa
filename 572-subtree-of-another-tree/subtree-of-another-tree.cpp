504b7a1622c2ca310789a6f33573b0357b40dd34
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-05-16 18:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (38.64%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSame(TreeNode* a, TreeNode* b) {
+        if (a == NULL && b == NULL) return true;
+        if (a == NULL || b == NULL) return false;
+
+        if (a->val != b->val) return false;
+
+        return isSame(a->left, b->left) && isSame(a->right, b->right);
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (subRoot == NULL) return true;
+        if (root == NULL) return false;
+
+        if (isSame(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
