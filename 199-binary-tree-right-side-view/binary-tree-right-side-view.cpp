cfccd21a51cf44003b8c757ff3adfd8ebf8c199a
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-05-21 17:41:45 +0530

    Time: 2 ms (9.81%) | Memory: 15.1 MB (40.18%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -18,10 +18,10 @@ public:
         q.push(root);
         while(!q.empty()){
             int size = q.size();
-            for(int i=1; i<=size; i++){
+            for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                if(i == size){
+                if(i == size - 1){
                     ans.push_back(node->val);
                 }
                 if(node->left){
