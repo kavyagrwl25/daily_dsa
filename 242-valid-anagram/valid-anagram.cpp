9009dc3b77f65eb3a2d0416c71b8b6c9ac658816
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-06-27 14:05:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>freq(26,0);
+        vector<int> v(26, 0);
+        if(s.length() != t.length()) return false;
+        for(char c : s){
+            v[c - 'a']++;
+        }
+        for(char c : t){
+            v[c - 'a']--;
+        }
+        for(int i =0; i<v.size(); i++){
+            if(v[i] != 0) return false;
+        }
+        return true;
+    }
+};
+
+
+//bute-> sort the string and compare
+//better> use unordered_map and store key and value
+
+
+
+
+
+
+
+
+/* 
+
+vector<int>freq(26,0);
         for(int ch: s){
             freq[ch-'a']++;
         }
@@ -12,5 +40,4 @@ public:
             if(count != 0) return false;
         }
         return true;
-    }
-};
+    } */
