dc2f23719d4680f2f4d59ea6446f88e7114160d2
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-06-27 14:42:50 +0530

    Time: 3 ms (66.92%) | Memory: 14.9 MB (18.87%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,7 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
+        unordered_map<int, int> u;
+        for(int i = 0; i<nums.size(); i++){
+            int complement = target - nums[i];
+            if(u.find(complement) != u.end()){
+                return {u[complement], i};
+            }
+            u[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+
+// brute-> use nested loops (O(N2))
+// better-> sort and two pointer but looses real indexes
+// best-> use unordered_map find complement in hashmap (O(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* unordered_map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
             int comp = target - nums[i];
             if(mp.find(comp) != mp.end()){
@@ -10,5 +45,4 @@ public:
             mp[nums[i]] = i;
         }
         return {};
-    }
-};
+    } */
