90185418bea590d95f1d5453a3a10acb205c3b7a
Kavya Agrawal <kavyaagarwal8273@gmail.com>
2026-06-27 13:09:26 +0530

    Time: 75 ms (12.51%) | Memory: 111.3 MB (5.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,11 +2,43 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       unordered_set<int> set;
+        for(int i =0; i<nums.size(); i++){
+            if(set.find(nums[i]) != set.end()){
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+    return false;
+    }
+};
+
+
+// brute-> use nested loop and check for all elements if similar element present
+// better> i can sort the array & check if next element is similar as current one (O(n log n))
+// best> use hashmap
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+ sort(nums.begin(),nums.end());
         bool flag = false;
         for(int i =0;i<nums.size()-1;i++){
             if(nums[i] == nums[i+1]) return true;
         }
-        return flag;
-    }
-};
+        return flag; */
